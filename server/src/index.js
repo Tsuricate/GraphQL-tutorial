3130616b6c56b10ed3e7566c72fd3732e0dff0cd
Tsukyko <zoeamat.pro@gmail.com>
2022-07-27 11:05:57 +0200

    GraphQL tutorial complete

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,22 @@
-//TODO
+const { ApolloServer } = require("apollo-server");
+const resolvers = require("./resolvers");
+const TrackAPI = require("./datasources/track-api");
+const typeDefs = require("./schema");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => {
+    return {
+      trackAPI: new TrackAPI(),
+    };
+  },
+});
+
+server.listen().then(() => {
+  console.log(`
+    🚀  Server is running!
+    🔉  Listening on port 4000
+    📭  Query at http://localhost:4000
+  `);
+});
